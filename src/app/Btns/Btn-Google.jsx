c06451594056500e7559abfb481fcacc4e7eb54a
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-09 13:46:03 -0400

    Se agrego un boton con el icono de Google

---
+++ src/app/Btns/Btn-Google.jsx
@@ -0,0 +1,9 @@
+import { FaGoogle } from "react-icons/fa";
+
+export const BtnGoogle = () => {
+    return(
+        <button className="border border-gray-300 text-gray-800 p-2 rounded-[100%] text-2xl">
+            <FaGoogle />
+        </button>
+    )
+}
