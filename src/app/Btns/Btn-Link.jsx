8680abae2db6d20134497ff3c4337969ef26f458
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-09 13:46:33 -0400

    Se agrego un boton con el icono de LinkedIn

---
+++ src/app/Btns/Btn-Link.jsx
@@ -0,0 +1,9 @@
+import { FaLinkedinIn } from "react-icons/fa";
+
+export const BtnLink = () => {
+    return(
+        <button className="border border-gray-300 text-gray-800 p-2 rounded-[100%] text-2xl">
+            <FaLinkedinIn/>
+        </button>
+    )
+}
