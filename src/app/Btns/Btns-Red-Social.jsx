e7ef30ebe31072df9d28a62f398fa347b26b8f38
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-09 13:47:05 -0400

    Se agrego un componente que contrendra todo los botones de la redes sociales

---
+++ src/app/Btns/Btns-Red-Social.jsx
@@ -0,0 +1,20 @@
+import { BtnFacebook } from "./Btn-Facebook";
+import { BtnGoogle } from "./Btn-Google";
+import { BtnLink } from "./Btn-Link";
+
+export default function Btns_Red_Social() {
+  return (
+    <div className="flex gap-2">
+        <BtnFacebook/>
+        <BtnGoogle/>
+        <BtnLink/>
+    </div>
+  )
+}
+
+
+
+
+
+
+
