40b3e4ca1fc582856dcd342d91b6370b061a4f18
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-09 13:45:38 -0400

    Se agrego un boton con el icono de facebook

---
+++ src/app/Btns/Btn-Facebook.jsx
@@ -0,0 +1,9 @@
+import { FaFacebookF } from "react-icons/fa";4
+
+export const BtnFacebook = () => {
+    return(
+        <button className="border border-gray-300 text-gray-800 p-2 rounded-[100%] text-2xl ">
+            <FaFacebookF/>
+        </button>
+    )
+}
